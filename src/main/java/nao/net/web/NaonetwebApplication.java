022990cc0593b71378501f45a0e378dcf7c86f84
ume <naonet.dorahhe@gmail.com>
2019-02-13 20:44:48 +0900

    commit

---
+++ src/main/java/nao/net/web/NaonetwebApplication.java
@@ -0,0 +1,14 @@
+package nao.net.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaonetwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaonetwebApplication.class, args);
+	}
+
+}
+
